a6e6f27b5ca26617f0596b96997d1a38b2665a66
mowaisnizami <mowaisnizami@gmail.com>
2025-07-04 22:43:22 +0500

    added shared module and basic components

---
+++ src/app/shared/components/delete-confirmation-popup/delete-confirmation-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirmation-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-confirmation-popup.component.html',
+  styleUrl: './delete-confirmation-popup.component.scss'
+})
+export class DeleteConfirmationPopupComponent {
+
+}
