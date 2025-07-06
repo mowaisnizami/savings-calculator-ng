97908306ab705b516582610af1a51bdd562f6eed
mowaisnizami <mowaisnizami@gmail.com>
2025-07-07 00:15:04 +0500

    working on styling

---
+++ src/app/shared/modules/primeng/primeng.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ButtonModule],
+  exports: [ButtonModule],
+})
+export class PrimengModule {}
